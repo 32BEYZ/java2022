5cde9264944e9b15d291daf6121e937f259043f9
ABİDE BEYZA BAYRAM <abidebeyzabayram@icloud.com>
2022-09-15 17:42:26 +0300

    Initial commit

---
+++ src/ödev1/beyza.java
@@ -0,0 +1,44 @@
+package ödev1;
+
+public class beyza {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	 // evin giriş noktası maindir.
+		System.out.println("Merhaba java");
+		System.out.println("Merhaba java1");
+		// case sensetive java büyük küçük harf duyarlı bir yazılım dilidir.
+		
+		
+		
+		
+		int oğrencıSayısı = 11;
+		String mesaj = "Öğrenci sayısı : ";
+		System.out.println("Öğrenci sayım = "+ oğrencıSayısı ); 
+		System.out.println("Öğrenci sayım = 10");
+	     System.out.println("Öğrenci sayım = 10");
+	     System.out.println("Öğrenci sayım = 10");	
+	     System.out.println( mesaj  + oğrencıSayısı); 
+	     
+	     System.out.println(mesaj  +  oğrencıSayısı ); 
+	     
+	     int sayı = 40; 
+	     if (sayı<20) {
+	    	 System.out.println("sayı 20 den küçüktür");
+	     }else if (sayı==20){                                                                                                    
+	    	 System.out.println("sayı 20 ye eşittir");
+	     }else {
+	    	 System.out.println("sayı 20 den büyükür");
+	    	 
+	    	 
+	     }
+	    
+	
+	
+	
+	
+	}
+
+}
